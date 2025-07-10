2005c00be179a563784b41b47ddc77ae684d20d8
Rob Rust <bobby.rust121@gmail.com>
2025-07-10 12:16:30 -0400

    update receiver address for mailgun

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -4,28 +4,29 @@ const mailgun = new Mailgun(FormData);
 
 console.log(process.env.MAILGUN_API_KEY);
 const mg: Interfaces.IMailgunClient = mailgun.client({
-    username: "api",
-    key: process.env.MAILGUN_API_KEY ?? "",
+	username: "api",
+	key: process.env.MAILGUN_API_KEY ?? "",
 });
 
 export async function POST(request: Request) {
-    const { name, email, subject, body } = await request.json();
+	const { name, email, subject, body } = await request.json();
 
-    const msg = await mg.messages
-        .create("sandbox139c104952194a1eaffd7f73b412344c.mailgun.org", {
-            from: "robrustdev@gmail.com",
-            to: ["bobby.rust121@gmail.com"],
-            subject: subject,
-            text: body + "\nName: " + name + "\nEmail: " + email,
-        })
-        .catch((err) => {
-            console.log(err);
-            return err;
-        });
+	const msg = await mg.messages
+		.create("sandbox139c104952194a1eaffd7f73b412344c.mailgun.org", {
+			from: "robrustdev@gmail.com",
+			to: ["contact@robrust.dev"],
+			subject: subject,
+			text: body + "\nName: " + name + "\nEmail: " + email,
+		})
+		.catch((err) => {
+			console.log(err);
+			return err;
+		});
 
-    if (msg instanceof Error) {
-        return new Response(JSON.stringify({ data: msg }), { status: 500 });
-    }
+	if (msg instanceof Error) {
+		console.log(msg);
+		return new Response(JSON.stringify({ data: msg }), { status: 500 });
+	}
 
-    return new Response(JSON.stringify({ data: msg }), { status: 200 });
+	return new Response(JSON.stringify({ data: msg }), { status: 200 });
 }
