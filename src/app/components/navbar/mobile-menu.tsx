afc6456b5dda9c65dac04192db4865ea87526919
Rob Rust <bobby.rust121@gmail.com>
2025-10-15 12:06:00 -0400

    fix mobile navbar invisible

--- src/app/components/navbar/mobile-menu.tsx
+++ src/app/components/navbar/mobile-menu.tsx
@@ -14,14 +14,14 @@ export default function MobileMenu() {
             {open && (
                 <div className="absolute top-0 left-0 w-screen h-screen bg-background">
                     <button onClick={() => setOpen(false)}>
-                        <X className="absolute top-3 right-3" size={30} />
+                        <X className="absolute top-3 right-3 text-black" size={30} />
                     </button>
-                    <ul className="flex flex-col justify-center gap-4 border-t-2 border-b-2 border-gray p-5 mt-6">
-                        <Link href="#about">About</Link>
-                        <Link href="#projects">Projects</Link>
-                        <Link href="#services">Services</Link>
-                        <Link href="#pricing">Pricing</Link>
-                        <Link href="#contact">Contact</Link>
+                    <ul className="flex flex-col justify-center text-black gap-4 border-t-2 border-b-2 border-gray p-5 mt-6">
+                        <Link href="#about" onClick={() => setOpen(false)}>About</Link>
+                        <Link href="#projects" onClick={() => setOpen(false)}>Projects</Link>
+                        <Link href="#services" onClick={() => setOpen(false)}>Services</Link>
+                        <Link href="#pricing" onClick={() => setOpen(false)}>Pricing</Link>
+                        <Link href="#contact" onClick={() => setOpen(false)}>Contact</Link>
                     </ul>
                 </div>
             )}
